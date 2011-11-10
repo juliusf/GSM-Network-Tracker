70b738bdedf2405c5b369d032b7c2d1b76e1bfda
Julius F <julius.flohr@gmail.com>
2011-11-11 00:34:31 +0100

    Map stitching working

--- dataView/tiles.py
+++ dataView/tiles.py
@@ -1,6 +1,7 @@
 import math
 import urllib
 import Image, ImageDraw
+import sys
 def deg2num(lat_deg, lon_deg, zoom):
  lat_rad = math.radians(lat_deg)
  n = 2.0 ** zoom
@@ -26,29 +27,57 @@ def getTileUrl(x, y, zoom):
  return 'http://tile.openstreetmap.org/' + str(zoom) +  '/' + str(x) + '/' + str(y) + '.png'
 
 def getImage(x,y,zoom):
- localfile = str( str(x) + '-' + str(y) + '.png')
- print('Getting image: ' + getTileUrl(x,y,zoom))
+ localfile = str(str(zoom) + '-' + str(x) + '-' + str(y) + '.png')
+ #print('Getting image: ' + getTileUrl(x,y,zoom))
  getFileFromWeb(getTileUrl(x,y,zoom),localfile)
  
 def getSeveralImages(latMin, latMax, lonMin, lonMax, zoom):
- print(str(lonMin))
- print(str(lonMax))
- print(str(latMin))
- print(str(latMax))
 
-
-
- lonMin, latMin = deg2num(lonMin, latMin, zoom)
- lonMax, latMax = deg2num(lonMax, latMax, zoom)
+ xMin, yMin = deg2num(latMin, lonMin, zoom)
+ xMax, yMax = deg2num(latMax, lonMax, zoom)
   
- print('getting From Lat: ' + str(latMin) + ' - ' + str(latMax))
- print('getting From Long: ' + str(lonMin) + ' - ' + str(lonMax))
- a = lonMin
- b = latMax
- while a <= lonMax:
-  while b <= latMin:
+ a = xMin
+ b = yMax
+ nr = 0
+ while a <= xMax:
+  while b <= yMin:
+   nr +=1
+   print"Download: %d of %d" % (nr, (xMax-xMin)*(yMin-yMax) + 1)
    getImage(a,b,zoom)
-   print (str(b) + '-' + str(latMin))
    b += 1
- a+=1
+  else: 
+   b = yMax
+   a +=1
+#Switch yMin/yMax order. This is introduced by the conversion from Lat / Long to X/Y-Coordinate System. 
+ temp = yMin
+ yMin = yMax
+ yMax = temp
+ return xMin,xMax,yMin,yMax
+
+def stitchImages(xMin, xMax,yMin,yMax,zoom):
+ dimX=(xMax - xMin + 1)*256
+ dimY=(yMax - yMin + 1)*256
+ print'Dimensions: dimX: %d dimY: %d' % (dimX,dimY)	
+ finalImage = Image.new("RGB", (dimX,dimY))
  
+ xOffset = 0;
+ yOffset = 0;
+ a = xMin
+ b = yMin
+ while a <= xMax:
+  while b <=yMax:
+   tempImage = Image.open( str(zoom) + '-' + str(a) + '-' + str(b) + '.png')
+   finalImage.paste(tempImage, (xOffset,yOffset))
+   print"pasted: x=%d y=%d xOffset=%d yOffset=%d" % (a,b,xOffset,yOffset)
+   yOffset += 256
+   b +=1
+  else:
+   xOffset += 256
+   yOffset = 0
+   b=yMin
+   a+= 1
+ 
+ finalImage.save('out.png', 'PNG')
+
+
+
